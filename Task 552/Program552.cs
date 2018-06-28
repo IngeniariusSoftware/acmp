f3f4fa5c894889083d537d5eb4616982953a6f5b
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-06-28 14:12:37 +0500

    Add solution of task 552

---
+++ Task 552/Program552.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task_552
+{
+    class Program552
+    {
+        static void Main()
+        {
+            Int64 countAnimalsTypes = Int64.Parse(Console.ReadLine());
+            string[] numbers = Console.ReadLine().Split(' ');
+            int[] countAnimals = new int[numbers.Length];
+            for (int index = 0; index < numbers.Length; index++)
+            {
+                countAnimals[index] = int.Parse(numbers[index]);
+            }
+
+            Int64[] combinations = new Int64[3];
+            if (countAnimalsTypes > 2)
+            {
+                for (int index = 0; index < countAnimalsTypes; index++)
+                {
+                    combinations[2] = combinations[2] + combinations[1] * countAnimals[index];
+                    combinations[1] = combinations[1] + combinations[0] * countAnimals[index];
+                    combinations[0] = combinations[0] + countAnimals[index];
+                }
+            }
+
+            Console.WriteLine(combinations[2]);
+        }
+    }
+}
