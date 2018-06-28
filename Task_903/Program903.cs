7ef61f87bea6a42838f986bc439617463bcfab81
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-06-28 13:39:10 +0500

    Add solution of task 903

--- Task_903/Program903.cs
+++ Task_903/Program903.cs
@@ -1,15 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task_903
 {
-    class Program
+    class Program903
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine(int.Parse(Console.ReadLine()) + 1);
         }
     }
 }
