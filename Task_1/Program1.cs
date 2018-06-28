fa6fe506a7a0a4d2d542b119e3d916c633b551ad
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-06-28 13:31:02 +0500

    Add solution of task 1

---
+++ Task_1/Program1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program1
+    {
+        static void Main()
+        {
+            string[] tokens = Console.ReadLine().Split(' ');
+            Console.WriteLine(int.Parse(tokens[0]) + int.Parse(tokens[1]));
+        }
+    }
+}
