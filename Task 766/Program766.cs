7632559c4ce56a0b4592f285b64fd6483662b3f5
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-06-28 14:36:39 +0500

    Add solution of task 766

---
+++ Task 766/Program766.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task_766
+{
+    class Program766
+    {
+        static void Main()
+        {
+            string[] tokens = Console.ReadLine().Split(' ');
+            if (int.Parse(tokens[0]) * int.Parse(tokens[1]) < int.Parse(tokens[2]))
+            {
+                Console.WriteLine("NO");
+            }
+            else
+            {
+                Console.WriteLine("YES");
+            }
+        }
+    }
+}
