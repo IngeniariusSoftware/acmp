d56dec3a0eacd8088a7fad39e2d607d7703acb0f
IngeniariusSoftware <kyngfy-panda@yandex.ru>
2018-07-02 21:18:47 +0500

    Task 72 is solved

--- Task 72/Program72.cs
+++ Task 72/Program72.cs
@@ -6,60 +6,57 @@ namespace Task_072
     {
         static void Main(string[] args)
         {
-            while (true)
+            char[] sequence = Console.ReadLine().ToCharArray();
+            int length = sequence.Length;
+            bool wasExchange = false;
+            if (length > 1)
             {
-                char[] sequence = Console.ReadLine().ToCharArray();
-                int length = sequence.Length;
-                bool wasExchange = false;
-                if (length > 1)
+                for (int index = length - 1; index > 0 && !wasExchange; index--)
                 {
-                    for (int index = length - 1; index > 0 && !wasExchange; index--)
+                    if (sequence[index] > sequence[index - 1])
                     {
-                        if (sequence[index] > sequence[index - 1])
+                        char shelf = sequence[index - 1];
+                        int min = index;
+                        for (int indexExchange = index + 1; indexExchange < length; indexExchange++)
                         {
-                            char shelf = sequence[index - 1];
-                            int min = index;
-                            for (int indexExchange = index + 1; indexExchange < length; indexExchange++)
+                            if (sequence[min] >= sequence[indexExchange]
+                                && sequence[indexExchange] > sequence[index - 1])
                             {
-                                if (sequence[min] > sequence[indexExchange]
-                                    && sequence[indexExchange] > sequence[index - 1])
-                                {
-                                    min = indexExchange;
-                                }
+                                min = indexExchange;
                             }
-
-                            sequence[index - 1] = sequence[min];
-                            sequence[min] = shelf;
-                            for (int indexInvert = index; indexInvert < (length - index) / 2 + index; indexInvert++)
-                            {
-                                char changeShelf = sequence[indexInvert];
-                                sequence[indexInvert] = sequence[length - 1 - indexInvert + index];
-                                sequence[length - 1 - indexInvert + index] = changeShelf;
-                            }
-
-                            wasExchange = true;
                         }
-                    }
 
-                    if (!wasExchange)
-                    {
-                        for (int indexInvert = 0; indexInvert < length / 2; indexInvert++)
+                        sequence[index - 1] = sequence[min];
+                        sequence[min] = shelf;
+                        for (int indexInvert = index; indexInvert < (length - index) / 2 + index; indexInvert++)
                         {
                             char changeShelf = sequence[indexInvert];
-                            sequence[indexInvert] = sequence[length - 1 - indexInvert];
-                            sequence[length - 1 - indexInvert] = changeShelf;
+                            sequence[indexInvert] = sequence[length - 1 - indexInvert + index];
+                            sequence[length - 1 - indexInvert + index] = changeShelf;
                         }
-                    }
 
-                    foreach (char element in sequence)
-                    {
-                        Console.Write(element);
+                        wasExchange = true;
                     }
                 }
-                else
+
+                if (!wasExchange)
                 {
-                    Console.WriteLine(sequence[0]);
+                    for (int indexInvert = 0; indexInvert < length / 2; indexInvert++)
+                    {
+                        char changeShelf = sequence[indexInvert];
+                        sequence[indexInvert] = sequence[length - 1 - indexInvert];
+                        sequence[length - 1 - indexInvert] = changeShelf;
+                    }
                 }
+
+                foreach (char element in sequence)
+                {
+                    Console.Write(element);
+                }
+            }
+            else
+            {
+                Console.WriteLine(sequence[0]);
             }
         }
     }
